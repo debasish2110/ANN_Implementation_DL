b755574e8055ba64e877da81b799799aa5f40bf3
debasish2110 <debasishdash98@gmail.com>
2021-10-28 23:24:28 +0530

    common.py updated

--- src/utils/common.py
+++ src/utils/common.py
@@ -2,5 +2,8 @@ import yaml
 
 def read_config(config_path):
     with open(config_path) as config_file:
-        content = yaml.safe_load(config_file)
-    return content
+        try:
+            content = yaml.safe_load(config_file)
+            return content
+        except yaml.YAMLError as exc:
+            print(exc)
