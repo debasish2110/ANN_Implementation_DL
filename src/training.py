e4d3d2d29dec8d1ae09ea4ab96d3255200615e43
debasish2110 <debasishdash98@gmail.com>
2021-10-29 00:51:13 +0530

    Good night

--- src/training.py
+++ src/training.py
@@ -1,6 +1,7 @@
 import os
 from utils.common import read_config
 from utils.data_Management import get_data
+from utils.model import create_model
 import argparse
 
 def training(config_path):
@@ -12,6 +13,17 @@ def training(config_path):
     validation_size = config['params']['validation_data_size']
     (x_train, y_train), (x_valid, y_valid), (x_test, y_test) = get_data(validation_size)
 
+    LOSS_FUNCTION=config['params']['loss_function']
+    OPTIMIZER=config['params']['optimizer']
+    METRICS=config['params']['metrics']
+    NUM_CLASSES=config['params']['num_classes']
+    
+    ann_model = create_model(LOSS_FUNCTION, OPTIMIZER, METRICS, NUM_CLASSES)
+
+    EPOCHS=config['params']['epochs']
+    VALIDATION_SET=(x_valid, y_valid)
+
+    history = ann_model.fit(x_train, y_train, epochs=EPOCHS, validation_data=VALIDATION_SET)
 
 
 if __name__ == '__main__':
